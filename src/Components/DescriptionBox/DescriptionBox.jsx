244555bdba57e57388fa57dc1306a552ea4f227f
Ahmedhanafy4199 <56001036+Ahmedhanafy4199@users.noreply.github.com>
2024-01-24 23:54:40 +0200

    first Uploading

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Review(122)</div>
+        </div>
+        <div className="descriptionbox-decription">
+            <p>
+                An e-commerce Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae quibusdam alias cupiditate officia dolore laudantium ipsum beatae ut? Odio, voluptas? Ducimus molestiae reiciendis consequuntur perferendis, nostrum, officia rem laudantium harum voluptas magni, temporibus facere velit.
+            </p>
+            <p>
+                E-commerce websites Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus veritatis dolore consectetur, odio repellat laboriosam cupiditate, saepe, excepturi repudiandae temporibus suscipit eveniet optio nulla quis. Cum nisi in eveniet fugit!
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
